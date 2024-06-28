d84be8f170c668e218855bef9df7f5a06e57c9fe
LoreRomark <lorenagby@gmail.com>
2024-06-27 19:17:41 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Show and close menu ------------------------------------------------
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.add('active');
+}
+document.querySelector('#close-btn').onclick = () => {
+    navbar.classList.remove('active');
+}
+
+
+// Carrousel Pagination
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
